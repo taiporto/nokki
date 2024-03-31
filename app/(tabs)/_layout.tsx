d3450d8a39e07cacd55571f4f3c638af053cceb3
Tai Porto <moonkoala23@gmail.com>
2024-03-31 13:13:21 -0300

    first commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Cartões",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              size={28}
+              name="cards-playing-outline"
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
