680f7595195eb208b58a22d7b6b8ce06f0a5a667
Tai <tai.dporto@gmail.com>
2024-12-22 23:25:59 -0300

    feat: create base profile screen

--- app/(app)/(tabs)/(profile)/index.tsx
+++ app/(app)/(tabs)/(profile)/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
-import Button from "../../_components/Button";
-import supabase from "../../../lib/supabase";
+import Button from "../../../_components/Button";
+import supabase from "../../../../lib/supabase";
 import { router } from "expo-router";
 
 export default function Profile() {
   return (
     <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-      <Text>Profile</Text>
       <Button
         onPress={() => {
           supabase.auth.signOut();
